64a327b16d5016edef29b57395b4505a08093145
forumber <kidobeko@gmail.com>
2013-02-04 18:03:52 +0200

    u8800: update RIL blobs

--- u8800-vendor-blobs.mk
+++ u8800-vendor-blobs.mk
@@ -74,6 +74,7 @@ PRODUCT_COPY_FILES += \
 	vendor/huawei/u8800/proprietary/lib/libcommondefs.so:system/lib/libcommondefs.so \
 	vendor/huawei/u8800/proprietary/lib/libdiag.so:system/lib/libdiag.so \
 	vendor/huawei/u8800/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
+	vendor/huawei/u8800/proprietary/lib/libdl.so:system/lib/libdl.so \
 	vendor/huawei/u8800/proprietary/lib/libdsm.so:system/lib/libdsm.so \
 	vendor/huawei/u8800/proprietary/lib/libdss.so:system/lib/libdss.so \
 	vendor/huawei/u8800/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
@@ -92,6 +93,7 @@ PRODUCT_COPY_FILES += \
 	vendor/huawei/u8800/proprietary/lib/libqmi.so:system/lib/libqmi.so \
 	vendor/huawei/u8800/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
 	vendor/huawei/u8800/proprietary/lib/libqueue.so:system/lib/libqueue.so \
+	vendor/huawei/u8800/proprietary/lib/libril.so:obj/lib/libril.so \
 	vendor/huawei/u8800/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
 	vendor/huawei/u8800/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
 	vendor/huawei/u8800/proprietary/lib/libwms.so:system/lib/libwms.so \
